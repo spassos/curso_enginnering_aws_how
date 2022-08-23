f22bdd18c89ed1a289e213d2b9962d7cda8c4c0e
Sergio Passos <sergio.passos88@gmail.com>
2022-08-23 10:22:09 -0300

    tests x jenkins

--- A007/cambio/main.py
+++ A007/cambio/main.py
@@ -2,12 +2,17 @@
 import requests
 import json
 from datetime import date, datetime
+import time
 # %%
 def cotacao(valor):
     url = f'https://economia.awesomeapi.com.br/last/USD-BRL'
     ret = requests.get(url)
     dolar = json.loads(ret.text)['USDBRL']
     return (float(dolar['bid']) * valor)
+
+moeda = cotacao(1)
+time.sleep(5)
+print("Cotacao atual: {}".format(moeda))
 # %%
 with open('cambio.csv', 'a') as f:
     f.write(
